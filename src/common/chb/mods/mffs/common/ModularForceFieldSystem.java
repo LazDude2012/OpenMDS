086d8fbdc5292ba5cdf456f258b8dd36fee67e7a
Thunderdark <Thunderdark@t-online.de>
2012-11-08 15:14:43 +0100

    Update to 2.2.8.0.11

--- src/common/chb/mods/mffs/common/ModularForceFieldSystem.java
+++ src/common/chb/mods/mffs/common/ModularForceFieldSystem.java
@@ -67,8 +67,8 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
 
-@Mod(modid = "ModularForceFieldSystem", name = "Modular ForceField System", version ="2.2.8.0.10")
-@NetworkMod(versionBounds = "[2.2.8.0.10]",clientSideRequired=true, serverSideRequired=false, clientPacketHandlerSpec = @NetworkMod.SidedPacketHandler(channels = {"MFFS" }, packetHandler = NetworkHandlerClient.class), serverPacketHandlerSpec = @NetworkMod.SidedPacketHandler(channels = {"MFFS" }, packetHandler = NetworkHandlerServer.class))
+@Mod(modid = "ModularForceFieldSystem", name = "Modular ForceField System", version ="2.2.8.0.11")
+@NetworkMod(versionBounds = "[2.2.8.0.11]",clientSideRequired=true, serverSideRequired=false, clientPacketHandlerSpec = @NetworkMod.SidedPacketHandler(channels = {"MFFS" }, packetHandler = NetworkHandlerClient.class), serverPacketHandlerSpec = @NetworkMod.SidedPacketHandler(channels = {"MFFS" }, packetHandler = NetworkHandlerServer.class))
 
 public class ModularForceFieldSystem {
 	
